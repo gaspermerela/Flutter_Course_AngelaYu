d541914f1ea0c47d65921db050905f09d58e8987
gasper <gapimerela123@gmail.com>
2020-05-18 08:14:05 +0200

    BMI Calculator

---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const Color kActiveCardColour = Color(0xFF1D1E33);
+const Color kInactiveCardColour = Color(0xFF111328);
+const Color kBottomContainerColour = Color(0xFFEB1555);
+const double kMinHeight = 120.0;
+const double kMaxHeight = 220.0;
+
+const Color kActiveSliderColor = Colors.white; //Color(0xFFEB1555);
+const Color kInactiveSliderColor = Color(0xFF8D8E98);
+
+const kButtonColor = Color(0xFF4C4F5E);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTitleTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF24D876),
+);
+
+const kResultNumberTextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultBodyTextStyle = TextStyle(fontSize: 22.0);
